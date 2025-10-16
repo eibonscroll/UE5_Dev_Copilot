6ae352086be8dccfbcbcbfc8b394c9e4d1af5ec8
Mike Purvis <mikegpurvis@gmail.com>
2025-10-16 15:53:34 -0600

    updated to pydantic v2 in prep for langchain

--- agentic-copilot/app/src/settings.py
+++ agentic-copilot/app/src/settings.py
@@ -14,10 +14,12 @@ class Settings(BaseSettings):
 
     OPENAI_API_KEY: Optional[SecretStr] = None
 
+    # v2 config:
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False,
+        extra="ignore",
     )
 
 SETTINGS = Settings()
